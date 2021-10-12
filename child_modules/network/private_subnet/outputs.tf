c3657afcb51ce29f7f81e38cf206512588b49e52
nazy67 <nazykh67@gmail.com>
2021-10-12 00:20:42 -0500

    modified private subnet module

--- child_modules/network/private_subnet/outputs.tf
+++ child_modules/network/private_subnet/outputs.tf
@@ -1,7 +1,4 @@
 # private subnet id's
-output "privsub_1_id" {
-  value = aws_subnet.privsub[1].id
-}
-output "privsub_2_id" {
-  value = aws_subnet.privsub[2].id
+output "privsub_ids" {
+  value = aws_subnet.private_subnet[*].id
 }
