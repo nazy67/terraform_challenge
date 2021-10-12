e15382aba45326578644ef0fabfdb00df2adf70d
nazy67 <nazykh67@gmail.com>
2021-10-11 20:49:03 -0500

    modified vpc module

--- child_modules/vpc/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-output "public_subnet_ids" {
-  value = aws_subnet.public_subnet[*].id
-}
-output "private_subnets_ids" {
-  value = aws_subnet.private_subnet[*].id
-}
