82a38181243269b1ea0418226b1f994aa4ce1f94
nazy67 <nazykh67@gmail.com>
2021-10-12 10:32:02 -0500

    renamed pub rt assoc

---
+++ child_modules/network/public_route_table_association/variables.tf
@@ -0,0 +1,65 @@
+# providers variables
+variable "aws_region" {
+  type        = string
+  description = " aws region to deploy infra"
+  default = ""
+}
+
+# # subnet variables
+# variable "pub_subnet_id" {
+#   type        = string
+#   description = "cird blocks for public subnets"
+#   default     = ""
+# }
+
+# public subnet variables
+variable "public_subnet" {
+  type        = list(string)
+  description = "public subnets"
+  default     = []
+}
+
+# internet gateway variable
+variable "igw_id" {
+  type        = string
+  description = "internet gateway id"
+  default     = ""
+}
+
+# nat gateway variable
+variable "nat_gateway_id" {
+  type        = string
+  description = "route table cidr block"
+  default     = ""
+}
+
+variable "pub_route_table_id" {
+  type        = string
+  description = "cidr blocks for private subnets"
+  default     = ""
+}
+
+# tags variables
+variable "env" {
+  type        = string
+  description = "environment name"
+  default = ""
+}
+
+variable "managed_by" {
+  type        = string
+  description = "managed by tr or cfn"
+  default = ""
+}
+
+variable "owner" {
+  type        = string
+  description = "owner of the resources"
+  default = ""
+}
+
+variable "giturl" {
+  type        = string
+  description = "git url to the template"
+  default = ""
+}
