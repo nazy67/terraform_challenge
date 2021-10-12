98176f527d760eb0e93a12d9a000c8949279d473
nazy67 <nazykh67@gmail.com>
2021-10-12 12:45:58 -0500

    updated outputs.tf

--- child_modules/vpc/outputs.tf
+++ child_modules/vpc/outputs.tf
@@ -1,19 +1,19 @@
-output "vpc_id" {
+output "module_vpc_id" {
   value = aws_vpc.my_vpc.id
 }
 
-output "public_subnet_1" {
+output "module_public_subnet_1" {
   value = aws_subnet.public_subnet_1.id
 }
 
-output "public_subnet_2" {
+output "module_public_subnet_2" {
   value = aws_subnet.public_subnet_2.id
 }
 
-output "private_subnet_1" {
+output "module_private_subnet_1" {
   value = aws_subnet.private_subnet_1.id
 }
 
-output "private_subnet_2" {
+output "module_private_subnet_2" {
   value = aws_subnet.private_subnet_2.id
 }
