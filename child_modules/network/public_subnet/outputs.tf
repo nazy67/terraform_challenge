7c0c818cd0008e4007160c5db98a87d5777beadf
nazy67 <nazykh67@gmail.com>
2021-10-11 23:59:05 -0500

    fixed syntax error

--- child_modules/network/public_subnet/outputs.tf
+++ child_modules/network/public_subnet/outputs.tf
@@ -1,7 +1,7 @@
 # public subnet id's
 output "pubsub_1_id" {
-  value = aws_subnet.pubsub[1].id
+  value = aws_subnet.pubsub-1.id
 }
 output "pubsub_2_id" {
-  value = aws_subnet.pubsub[2].id
+  value = aws_subnet.pubsub-2.id
 }
