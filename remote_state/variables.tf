885cb098792f1d0056ee8c42b1e2a3f4f999d138
nazy67 <nazykh67@gmail.com>
2021-10-13 22:38:20 -0500

    updated remote state

--- remote_state/variables.tf
+++ remote_state/variables.tf
@@ -2,3 +2,8 @@ variable "account_id" {
     description = "account id"
     default = "974912841781" # enter your account id
 }
+
+variable "aws_region" {
+    description = "aws_region"
+    default = "us-east-1" # enter region
+}
